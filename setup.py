c19fa425fb892d601283c4ddb8b9118249aca127
ndelacour <nvanthof@free.fr>
2023-03-11 13:18:27 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from os import path
 here = path.abspath(path.dirname(__file__))
 
 setup(
-    name='ckanext-publications-qld-theme',
+    name='ckanext-safran_opendata_theme',
 
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
